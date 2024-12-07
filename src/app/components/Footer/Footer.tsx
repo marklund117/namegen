c4fd71cc5ec79139951b339393ced6c5434d4890
Mark Lund <marklund117@gmail.com>
2024-12-06 18:00:30 -0700

    why isnt tailwind working bruh

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import '../../styles/globals.css'
+
+export default function Footer() {
+    return <footer className="bg-black text-blue-700">
+        <div>
+        <h3>This is a Footer component</h3>
+        </div>
+    </footer>
+}

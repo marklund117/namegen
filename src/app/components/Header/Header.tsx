c8a5f32e38c1f71a198cb1c78dbfd0f56d4a69e3
Mark Lund <marklund117@gmail.com>
2024-12-15 13:52:51 -0700

    favicon + some other changes

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,9 +1,10 @@
 import '../../styles/globals.css'
 import Link from 'next/link'
+import Image from 'next/image'
 export default function Header() {
     return <header className="bg-neutral-900 text-emerald-300 flex justify-center shadow-md">
         <div className="p-4 flex justify-between lg:w-[1024px] w-full">
-        <Link href="/">Namegen</Link>
+        <Link href="/">Logo</Link>
         <Link href="/about">About</Link>
         </div>
     </header>
--- src/app/components/UsernameGenerator/UsernameGenerator.tsx
+++ src/app/components/UsernameGenerator/UsernameGenerator.tsx
@@ -4,12 +4,13 @@ import { Mistral } from '@mistralai/mistralai'
 
 // generic function to copy text of a specific button to clipboard
 function copyToClipboard(btnClass: string) {
-  const copyText = document.getElementsByClassName(btnClass)[0]
-  if (copyText) {
-    navigator.clipboard.writeText(copyText.innerHTML)
+  const buttonToCopy = document.getElementsByClassName(btnClass)[0]
+  if (buttonToCopy) {
+    navigator.clipboard.writeText(buttonToCopy.innerHTML)
   }
 }
 
+// typescript needs apiKey to be specified as a string
 interface GenProps {
   apiKey: string;
 }
@@ -20,14 +21,14 @@ export default function UsernameGenerator({ apiKey }: GenProps) {
     console.log('Generating usernames...')
 
     // create a Mistral instance
-    const client = new Mistral({apiKey: apiKey})
+    const client = new Mistral({ apiKey: apiKey })
 
     // get values from the customization panel (later)
 
 
     const chatResponse = await client.chat.complete({
       model: 'mistral-large-latest',
-      messages: [{role: 'user', content: 'list 10 unique internet usernames, separated by commas (comma only, no spaces or newlines). The usernames may optionally contain underscores and uppercase or lowercase letters but no spaces or special characters. No other text should be present in your response.'}]
+      messages: [{ role: 'user', content: 'list 10 unique internet usernames, separated by commas (comma only, no spaces or newlines). The usernames may optionally contain underscores and uppercase or lowercase letters but no spaces or special characters. No other text should be present in your response.' }]
     })
     if (chatResponse.choices && chatResponse.choices.length > 0) {
       console.log('Chat:', chatResponse.choices[0])
@@ -44,7 +45,7 @@ export default function UsernameGenerator({ apiKey }: GenProps) {
       // now for each element in the split array, we generate an element
       splitArray.forEach((username) => {
         const newName = document.createElement('button')
-      // and give each one a class name corresponding to their position in the list
+        // and give each one a class name corresponding to their position in the list
         newName.className = `username${splitArray.indexOf(username)} shadow-md p-2 rounded-md w-full bg-white border-2 border-solid border-emerald-300 hover:border-emerald-400 hover:bg-emerald-100`
         newName.innerHTML = username
         // and add a click handler to each
@@ -58,10 +59,10 @@ export default function UsernameGenerator({ apiKey }: GenProps) {
     }
   }
   return (
-      <div className="p-4 flex lg:w-[1024px] w-full m-auto flex-col items-center">
+    <div className="p-4 flex lg:w-[1024px] w-full m-auto flex-col items-center bg-white">
       <h1 className="mb-4 text-center">Namegen.space - Under Construction! Click a generated username to copy.</h1>
       <button className="p-4 w-full bg-emerald-300 rounded-md hover:bg-emerald-400 shadow-md" onClick={generateUsernames}>Generate Usernames</button>
       <div id="usernamebox" className="w-full mt-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4"></div>
-      </div>
+    </div>
   )
 }

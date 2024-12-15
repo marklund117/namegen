7edf7417371f7ff4439cfb1661a36a12e1a06478
Mark Lund <marklund117@gmail.com>
2024-12-14 17:31:06 -0700

    commentary only

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import UsernameGenerator from './components/UsernameGenerator/UsernameGenerator'
 
 // wait... if this is a server component this isn't visible to the client, right?
 const apiKey = process.env.MISTRAL_API_KEY || ''
+// lol it literally was that simple, need to learn more about ways to do this securely
 
 export default async function HomePage() {
   return <UsernameGenerator apiKey={apiKey} />

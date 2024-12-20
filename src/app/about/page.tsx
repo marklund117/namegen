694a3e0a6480706c2671c88a7cb2186b6e5363b1
Mark Lund <marklund117@gmail.com>
2024-12-20 16:32:07 -0700

    updating about page and README

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,10 @@
+import Link from 'next/link'
+
 export default function About() {
     return (
         <div className="p-4 flex lg:w-[1024px] w-full m-auto flex-col items-center">
-        <h1>About Page - Under Construction!</h1>
-        <p>This project uses Mistral AI for back-end requests</p>
+        <p className="mb-4">Namegen is a <Link href="https://nextjs.org/" className="text-emerald-400 underline">Next.js</Link> based web application which uses <Link href="https://mistral.ai/" className="text-emerald-400 underline">Mistral AI</Link> (specifically the mistral-large model) to generate unique usernames based on user input. The project is styled with <Link href="https://tailwindcss.com/" className="text-emerald-400 underline">Tailwind CSS</Link></p>
+        <p>For best results, specify only 1-2 keywords, to allow space for style and tone influence.</p>
         </div>
     )
   }
--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     return <header className="bg-neutral-900 text-emerald-300 flex justify-center shadow-md">
         <div className="p-4 flex flex-row justify-between align-middle lg:w-[1024px] w-full">
         <Link href="/" title="namegen.space"><Image src="/headerlogo.png" alt="Logo" width="32" height="32" /></Link>
-        <Link href="/about" className="font-mono text-xl mt-0.5">Development</Link>
+        <Link href="/about" className="font-mono text-xl mt-0.5">About</Link>
         </div>
     </header>
 }
